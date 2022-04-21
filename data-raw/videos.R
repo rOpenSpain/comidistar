e693bd7810b651824711ccd9d4eeccb1bc90db53
gongcastro <gonzalo.garciadecastro@upf.edu>
2022-04-21 10:13:33 +0200

    Fix corrupt rows in raw data

--- data-raw/videos.R
+++ data-raw/videos.R
@@ -1,7 +1,13 @@
 library(tibble)
 
 file_path <- system.file("extdata", "videos.txt", package = "comidistar")
-videos <- as_tibble(read.delim(file_path))
+videos <- as_tibble(
+    read.delim(
+        file_path,
+        skipNul = TRUE,
+        col.names = c("producto", "fecha", "link")
+    )
+)
 
 # load data
 usethis::use_data(videos, overwrite = TRUE)
